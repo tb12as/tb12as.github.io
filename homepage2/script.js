e5515f4c131b103c175bc899114b3c0f8c780eef
Syafiq Afifuddin <syafiqafifuddin59@gmail.com>
2020-08-21 01:44:45 +0700

    Tambahan ganti nama

--- homepage2/script.js
+++ homepage2/script.js
@@ -1,5 +1,7 @@
 const clock = document.querySelector('#clock');
 const greet = document.querySelector('#greet');
+const nameInput = document.querySelector('.name-input');
+var name = localStorage.name;
 function ready(fn) {
 	if (document.readyState == 'complete' || document.readyState == 'interactive') {
 		fn;
@@ -14,7 +16,7 @@ function padZero(data) {
 		return data;
 	}
 }
-function timeAndGreet() {
+function time() {
 	var now = new Date(Date.now());
 	var hour = padZero(now.getHours());
 	var minutes = padZero(now.getMinutes());
@@ -28,21 +30,6 @@ function timeAndGreet() {
 		clock.innerHTML = hour +':'+ minutes +':'+ seconds;
 	}, 1000);
 
-	if (now.getHours() >= 0 && now.getHours() < 11) {
-		greet.innerHTML = 'Selamat Pagi, Syafiq Afifuddin';
-		title = 'Pagi';
-	} else if(now.getHours() >= 11 && now.getHours() < 15) {
-		greet.innerHTML = 'Selamat Siang, Syafiq Afifuddin';
-		title = 'Siang';
-	}	else if(now.getHours() >= 15 && now.getHours() < 18) {
-		greet.innerHTML = 'Selamat Sore, Syafiq Afifuddin';
-		title = 'Sore';
-	} else {
-		greet.innerHTML = 'Selamat Malam, Syafiq Afifuddin';
-		title = 'Malam';
-	}
-	document.title = title;
-
 	// get date, note: ini tidak interval jadi ketika pergantian hari tidak akan berubah
 	var day = now.getDay(); // hari berdasarkan index array (mulai dari minggu)
 	var date = now.getDate();
@@ -50,8 +37,7 @@ function timeAndGreet() {
 	var year = now.getFullYear();
 	// note : index dimulai dari 0
 
-	var allDay = ["Minggu",	"Senin", "Selasa", "Rabu", "Kamis",	"Jum'at",	"Sabtu",
-	];
+	var allDay = ["Minggu",	"Senin", "Selasa", "Rabu", "Kamis",	"Jum'at", "Sabtu"];
 	var allMonth = [
 		"Januari",
 		"Februari",
@@ -74,6 +60,53 @@ function timeAndGreet() {
 	semuaHariDiDOM[day].classList.add('active');
 }
 
+function greetFunction(name) {
+	if(!name) {
+		name = 'User';
+	}
+	var now =  new Date(Date.now());
+	var title = '';
+	if (now.getHours() >= 0 && now.getHours() < 11) {
+		greet.innerHTML = `Selamat Pagi, ${name}`;
+		title = 'Pagi';
+	} else if(now.getHours() >= 11 && now.getHours() < 15) {
+		greet.innerHTML = `Selamat Siang, ${name}`;
+		title = 'Siang';
+	}	else if(now.getHours() >= 15 && now.getHours() < 18) {
+		greet.innerHTML = `Selamat Sore, ${name}`;
+		title = 'Sore';
+	} else {
+		greet.innerHTML = `Selamat Malam, ${name}`;;
+		title = 'Malam';
+	}
+	document.title = title;
+}
+
 ready(() => {
-	timeAndGreet();
+	time();
+
+	greetFunction(name);
+
+	greet.addEventListener('click', function(event) {
+		event.preventDefault();
+		this.classList.add('d-none');
+		nameInput.classList.remove('d-none');
+		nameInput.value = localStorage.name;
+		nameInput.select();
+		nameInput.focus();
+	});
+
+	function hideInput() {
+		nameInput.classList.add('d-none');
+		greet.classList.remove('d-none');
+		return greetFunction(localStorage.name);
+	}
+
+	nameInput.addEventListener('keyup', function(event) {
+		if (event.code == 'Enter') {
+			var nama = this.value;
+			localStorage.setItem('name', nama);
+			hideInput();
+		}
+	});
 });
