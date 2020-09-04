45e93e3ffc7d5ca5d3fa602092888c6b5bb396ae
Syafiq Afifuddin <syafiqafifuddin59@gmail.com>
2020-09-04 14:46:51 +0700

    Portfolio and some update

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+$(function() {
+	$('[data-toggle="tooltip"]').tooltip();
+
+	setTimeout(() => {
+		$('.socialmedia').css('margin', '0 10px');
+	}, 200);
+
+	setTimeout(() => {
+		$('.profile-img').css('opacity', 1);
+	}, 500);
+});
