fedae75fe529226bfc5e4eeb57c3efbdc07f931a
Syafiq Afifuddin <syafiqafifuddin59@gmail.com>
2020-09-11 02:21:41 +0700

    on ready animation homepage 2

--- homepage2/script.js
+++ homepage2/script.js
@@ -11,7 +11,7 @@ function ready(fn) {
 }
 function padZero(data) {
 	if (data < 10) {
-		return '0'+data;
+		return '0' + data;
 	} else {
 		return data;
 	}
@@ -21,13 +21,13 @@ function time() {
 	var hour = padZero(now.getHours());
 	var minutes = padZero(now.getMinutes());
 	var seconds = padZero(now.getSeconds());
-	clock.innerHTML = hour +':'+ minutes +':'+ seconds;
+	clock.innerHTML = hour + ':' + minutes + ':' + seconds;
 	setInterval(() => {
 		var now = new Date(Date.now());
 		var hour = padZero(now.getHours());
 		var minutes = padZero(now.getMinutes());
 		var seconds = padZero(now.getSeconds());
-		clock.innerHTML = hour +':'+ minutes +':'+ seconds;
+		clock.innerHTML = hour + ':' + minutes + ':' + seconds;
 	}, 1000);
 
 	// get date, note: ini tidak interval jadi ketika pergantian hari tidak akan berubah
@@ -37,7 +37,7 @@ function time() {
 	var year = now.getFullYear();
 	// note : index dimulai dari 0
 
-	var allDay = ["Minggu",	"Senin", "Selasa", "Rabu", "Kamis",	"Jum'at", "Sabtu"];
+	var allDay = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu"];
 	var allMonth = [
 		"Januari",
 		"Februari",
@@ -52,27 +52,51 @@ function time() {
 		"November",
 		"Desember"
 	];
-	var print = allDay[day]+', '+date+' '+allMonth[month]+' '+year;
+	var print = allDay[day] + ', ' + date + ' ' + allMonth[month] + ' ' + year;
 	document.querySelector('#date').innerHTML = print;
 	// add class active to day now
-	var semuaHariDiDOM = Array.prototype.slice.call = (document.querySelector('.left-clock').children);
-	day = (day == 0 ? 6 : day -1); // use your brain to understand this
-	semuaHariDiDOM[day].classList.add('active');
+	setTimeout(() => {
+		var semuaHariDiDOM = Array.prototype.slice.call = (document.querySelector('.left-clock').children);
+		day = (day == 0 ? 6 : day - 1); // use your brain to understand this
+		semuaHariDiDOM[day].classList.add('active');
+	}, 300);
+	setTimeout(() => {
+		document.querySelector('.left').classList.add('m');
+	}, 50);
+	setTimeout(() => {
+		document.querySelector('.left-clock').classList.add('m');
+	}, 100);
+	setTimeout(() => {
+		document.querySelector('.center-clock').classList.add('m');
+	}, 350);
+	setTimeout(() => {
+		document.querySelector('.clock').classList.add('m');
+	}, 780);
+	setTimeout(() => {
+		document.querySelector('.right').classList.add('m');
+	}, 210 + 100 + 400 + 200);
+	setTimeout(() => {
+		document.querySelector('.btn').classList.add('m');
+		var title = document.querySelectorAll('.title');
+		title.forEach(e => {
+			e.classList.add('m');
+		});
+	}, 210 + 100 + 400 + 400);
 }
 
 function greetFunction(name) {
-	if(!name) {
-		name = 'User';
+	if (name == 'undefined') {
+		name = 'Tanpa Nama';
 	}
-	var now =  new Date(Date.now());
+	var now = new Date(Date.now());
 	var title = '';
 	if (now.getHours() >= 0 && now.getHours() < 11) {
 		greet.innerHTML = `Selamat Pagi, ${name}`;
 		title = 'Pagi';
-	} else if(now.getHours() >= 11 && now.getHours() < 15) {
+	} else if (now.getHours() >= 11 && now.getHours() < 15) {
 		greet.innerHTML = `Selamat Siang, ${name}`;
 		title = 'Siang';
-	}	else if(now.getHours() >= 15 && now.getHours() < 18) {
+	} else if (now.getHours() >= 15 && now.getHours() < 18) {
 		greet.innerHTML = `Selamat Sore, ${name}`;
 		title = 'Sore';
 	} else {
@@ -87,7 +111,7 @@ ready(() => {
 
 	greetFunction(name);
 
-	greet.addEventListener('click', function(event) {
+	greet.addEventListener('click', function (event) {
 		event.preventDefault();
 		this.classList.add('d-none');
 		nameInput.classList.remove('d-none');
@@ -102,7 +126,7 @@ ready(() => {
 		return greetFunction(localStorage.name);
 	}
 
-	nameInput.addEventListener('keyup', function(event) {
+	nameInput.addEventListener('keyup', function (event) {
 		if (event.code == 'Enter') {
 			var nama = this.value;
 			localStorage.setItem('name', nama);
